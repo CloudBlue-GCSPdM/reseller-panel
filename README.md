4f2cad05aa8e3700e585a228fe4990553f095de2
Andrés Garcia <Andres.GarciaRamirez@cloud.corp.im>
2022-01-20 10:11:02 +0100

    first commit, structure of api created

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# reseller-panel
-Python API sandbox for cybersecurity reseller's glass panel viability
+# Reseller-panel
+Python API sandbox for cyber security reseller's glass panel viability
+
+
+##How to run
+(recommmended to use venv for isolated dependencies environment)
+```
+#install
+pip install requirements.txt
+
+#run
+uvicorn main:app --reload
+
+```
---
+++ main.py
@@ -0,0 +1,30 @@
+from config import Api_key
+from connect.client import ConnectClient
+from typing import Optional
+from fastapi import FastAPI
+
+client = ConnectClient(Api_key)
+app = FastAPI()
+
+
+# test query
+def run_query():
+    products = client.products.all()
+
+    for product in products:
+        print("item-------")
+        print(product)
+
+
+@app.get("/")
+def read_root():
+    return {"test": "response"}
+
+
+@app.get("/products/{vendor_id}")
+def read_products(vendor_id: Optional[str] = None):
+    return {"vendor_id": vendor_id, "products": None}
+
+
+if __name__ == '__main__':
+    run_query()
